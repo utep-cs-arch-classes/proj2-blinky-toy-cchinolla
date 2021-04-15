1d55f7fb3a624b5ab528187d5b93e6485671c2b1
cchinolla <cmchinolla@miners.utep.edu>
2021-04-15 16:55:09 -0600

    final

--- project/switches.c
+++ project/switches.c
@@ -1,21 +1,17 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
-#include "stateMachines.c"
+//#include "stateMachines.h"
+//#include <libTimer.h>
 
-char state;
-char SW1down;
-char SW2down;
-char SW3down;
-char SW4down;
-char switch_state;
+char state, s1, s2, s3, s4;
 char switch_state_changed;
 
 static char switch_update_interrupt_sense(){
   char p2val = P2IN;
   //updare switch interrupt to detect changes from current buttons
   P2IES |= (p2val & SWITCHES);
-  P2IES &= (p2vval |= ~SWITCHES);
+  P2IES &= (p2val |= ~SWITCHES);
   return p2val;
 }
 
@@ -24,20 +20,26 @@ void switch_init(){
   P2IE |= SWITCHES;
   P2OUT |= SWITCHES;
   P2DIR &= ~SWITCHES;
-  switch_ipdate_interrupt_sense();
-  switch_interrupt_handler();
+  switch_update_interrupt_sense();
+  led_update();
 }
 
 void switch_interrupt_handler(){
   char p2val = switch_update_interrupt_sense();
 
-  if(p2val & SW! ? 0 : 1){
+  s1 = (p2val & SW1) ? 0 : 1;
+  s2 = (p2val & SW2) ? 0: 1;
+  s3 = (p2val & SW3) ? 0: 1;
+  s4 = (p2val & SW4) ? 0 : 1;
+
+  
+  if(s1){
     state = 1;
-  }else if(p2val & SW2 ? 0: 1){
+  }else if(s2){
     state = 2;
-  }else if(p2val & SW3 ? 0: 1){
+  }else if(s3){
     state = 3;
-  }else if(p2val & SW4 ? 0: 1){
+  }else if(s4){
     state = 4;
   }
   state_advance();
--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -6,13 +6,15 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 250) {
-    state_advance();
-    blink_count = 0;
-  }
-  static char song_count = 0;
-  if(++song_count == 250){
-    // state_advance();
-    song_count = 0;
-  }
+     if(state == 3 && ++blink_count == 1){
+       state_advance();
+       // dim();
+       blink_count = 0;
+     }else if(++blink_count == 250){
+       state_advance();
+       blink_count = 0;
+     }
+
+  
+    
 }
